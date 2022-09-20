da271b47e903f8b37742e52b6b2b82540ae0f70c
Isaac Lourenço <isaac.lourenco.704@ufrn.edu.br>
2022-09-20 10:56:51 -0300

    solicitarAumentoLimite e realizarCompra emitindo logs

--- src/br/ufrn/imd/dominio/EnumHistoricoStrings.java
+++ src/br/ufrn/imd/dominio/EnumHistoricoStrings.java
@@ -3,7 +3,11 @@ package br.ufrn.imd.dominio;
 public enum EnumHistoricoStrings {
     ERRO_SACAR_SALDO_INSUFICIENTE("Saldo insuficiente."),
     VALOR_SACADO_SUCESSO("Valor sacado com sucesso."),
-    VALOR_DEPOSITADO_SUCESSO("Valor depositado com sucesso: ");
+    VALOR_DEPOSITADO_SUCESSO("Valor depositado com sucesso: "),
+    ERRO_COMPRA("Compra não autorizada."),
+    COMPRA_SUCESSO("Compra realizada com sucesso."),
+    ERRO_AUMENTO_LIMITE("Aumento de limite não concedido."),
+    AUMENTO_LIMITE_SUCESSO("Aumento de limite concedido.");
 
     private String valor;
     EnumHistoricoStrings(String valorOpcao){
